c42189735693b757ac64dde105406b73fb348008
Stanislav <istaslis@gmail.com>
2014-11-10 10:51:30 +0100

    Code clean-up

--- RecoHI/HiTracking/python/hiDetachedStep_cff.py
+++ RecoHI/HiTracking/python/hiDetachedStep_cff.py
@@ -3,17 +3,6 @@ from HIPixelTripletSeeds_cff import *
 from HIPixel3PrimTracks_cfi import *
 from hiSecondPixelTripletStep_cff import *
 
-#hidetachedTripletStepClusters = detachedTripletStepClusters.clone()
-#hidetachedTripletStepSeedLayers = detachedTripletStepSeedLayers.clone()
-#hidetachedTripletStepSeeds = detachedTripletStepSeeds.clone()
-#hidetachedTripletStepTrackCandidates = detachedTripletStepTrackCandidates.clone()
-#hidetachedTripletStepTracks = detachedTripletStepTracks.clone()
-#hidetachedTripletStepSelector = detachedTripletStepSelector.clone()
-#hidetachedTripletQual = detachedTripletStep.clone()
-
-
-#?hidetachedTripletStepClusters.oldClusterRemovalInfo = cms.InputTag('hiPixelPairClusters')
-
 hiDetachedClusters = cms.EDProducer("HITrackClusterRemover",
      clusterLessSolution = cms.bool(True),
      trajectories = cms.InputTag("hiPixelPairGlobalPrimTracks"),
@@ -90,7 +79,6 @@ hiDetachedPixelTracks = cms.EDProducer("PixelTrackProducer",
 
 hiDetachedPixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.extraHitRPhitolerance = cms.double(0.0)
 hiDetachedPixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.extraHitRZtolerance = cms.double(0.0)
-#hiDetachedPixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.SeedingLayers = cms.InputTag('hiDetachedSeedLayers')
 hiDetachedPixelTracks.OrderedHitsFactoryPSet.SeedingLayers = cms.InputTag('hiDetachedSeedLayers')
 
 
@@ -114,10 +102,6 @@ hiDetachedPixelTracks.RegionFactoryPSet = cms.PSet(
 )
 hiDetachedPixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.maxElement = cms.uint32(1000000)
 
-#try tracks!!!
-#hidetachedPixelTracks = hiPixel3PrimTracks.clone()
-#hidetachedPixelTracks.OrderedHitsFactoryPSet = hidetachedTripletStepSeeds.OrderedHitsFactoryPSet
-#hidetachedPixelTracks.RegionFactoryPSet = hidetachedTripletStepSeeds.RegionFactoryPSet
 hiDetachedPixelTracks.FilterPSet.ptMin = cms.double(minPt)
 hiDetachedPixelTracks.FilterPSet.lipMax = cms.double(sv)
 hiDetachedPixelTracks.FilterPSet.tipMax = cms.double(sv)
@@ -128,45 +112,18 @@ hiDetachedPixelTracks.FilterPSet.useClusterShape = cms.bool(True)
 
 hiDetachedPixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.SeedComparitorPSet = RecoPixelVertexing.PixelLowPtUtilities.LowPtClusterShapeSeedComparitor_cfi.LowPtClusterShapeSeedComparitor
 
-
-#hidetachedPixelTrackSeeds = hiPixelTrackSeeds.clone()
-#hidetachedPixelTrackSeeds.InputCollection = cms.InputTag("hidetachedPixelTracks")
-#hiSecondPixelTripletTrackCandidates.src=cms.InputTag("hidetachedPixelTrackSeeds") #below
-
 import RecoPixelVertexing.PixelLowPtUtilities.TrackSeeds_cfi
 hiDetachedSeeds = RecoPixelVertexing.PixelLowPtUtilities.TrackSeeds_cfi.pixelTrackSeeds.clone(
         InputCollection = 'hiDetachedPixelTracks'
   )
 
-#hiDetachedSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = cms.uint32(50000000)
-#hiDetachedSeeds.ClusterCheckPSet.MaxNumberOfPixelClusters = cms.uint32(5000000)
-
-
-
-#????????hidetachedTripletStepSeeds.SeedComparitorPSet = .... cms.string('PixelClusterShapeSeedComparitor'),
-#change region and to trackseeds!
-
-#hidetachedTripletStepTrajectoryFilter = detachedTripletStepTrajectoryFilter.clone()
-#hidetachedTripletStepTrajectoryFilter.ComponentName='hidetachedTripletStepTrajectoryFilter'
-#hidetachedTripletStepTrajectoryFilter.filterPset.minPt = cms.double(minPt)
-
-#hidetachedTripletStepChi2Est = detachedTripletStepChi2Est.clone()
-#hidetachedTripletStepChi2Est.ComponentName = 'hidetachedTripletStepChi2Est'
-
-#hidetachedTripletStepTrajectoryBuilder = detachedTripletStepTrajectoryBuilder.clone()
-#hidetachedTripletStepTrajectoryBuilder.ComponentName='hidetachedTripletStepTrajectoryBuilder'
-#hidetachedTripletStepTrajectoryBuilder.trajectoryFilterName='hidetachedTripletStepTrajectoryFilter'
-#hidetachedTripletStepTrajectoryBuilder.estimator = 'hidetachedTripletStepChi2Est'
-
-
-
 # QUALITY CUTS DURING TRACK BUILDING
 import TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff
 hiDetachedTrajectoryFilter = TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff.CkfBaseTrajectoryFilter_block.clone(
-    maxLostHits = 999, #1?
+    maxLostHits = 999,
     minimumNumberOfHits = 6,
     minPt = cms.double(minPt),
-    constantValueForLostHitsFractionFilter = cms.double(0.701) #1.0?
+    constantValueForLostHitsFractionFilter = cms.double(0.701)
     )
 
 import TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi
@@ -177,22 +134,12 @@ hiDetachedChi2Est = TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProdu
         )
 
 
-
-
-#hidetachedTripletStepTrackCandidates.TrajectoryBuilder = cms.string('hidetachedTripletStepTrajectoryBuilder')
-#hidetachedTripletStepTrackCandidates.clustersToSkip='hidetachedTripletStepClusters'
-#hidetachedTripletStepTrackCandidates.src = cms.InputTag("hidetachedTripletStepSeeds")
-#hidetachedTripletStepTrackCandidates.src = cms.InputTag("hidetachedPixelTrackSeeds")
-
-#hidetachedTripletStepTracks.src = cms.InputTag('hidetachedTripletStepTrackCandidates')
-
-
 # TRACK BUILDING
 import RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilder_cfi
 hiDetachedTrajectoryBuilder = RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilder_cfi.GroupedCkfTrajectoryBuilder.clone(
     MeasurementTrackerName = '',
     trajectoryFilter = cms.PSet(refToPSet_ = cms.string('hiDetachedTrajectoryFilter')),
-    maxCand = 2, #3?
+    maxCand = 2,
     estimator = cms.string('hiDetachedChi2Est'),
     maxDPhiForLooperReconstruction = cms.double(0),
     maxPtForLooperReconstruction = cms.double(0),
@@ -222,15 +169,6 @@ hiDetachedTracks = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProducer.clo
     Fitter=cms.string('FlexibleKFFittingSmoother')
     )
 
-
-
-#hidetachedTripletStepSelector.vertices = cms.InputTag("hiSelectedVertex")
-#hidetachedTripletStepSelector.src=cms.InputTag("hidetachedTripletStepTracks")
-
-#for sel in hidetachedTripletStepSelector.trackSelectors:
-#    sel.name=cms.string('hi'+sel.name.value())
-#    if (sel.preFilterName.value()!=''): sel.preFilterName=cms.string('hi'+sel.preFilterName.value())
-
 # Final selection
 import RecoHI.HiTracking.hiMultiTrackSelector_cfi
 hiDetachedSelector = RecoHI.HiTracking.hiMultiTrackSelector_cfi.hiMultiTrackSelector.clone(
@@ -238,37 +176,21 @@ hiDetachedSelector = RecoHI.HiTracking.hiMultiTrackSelector_cfi.hiMultiTrackSele
     trackSelectors= cms.VPSet(
     RecoHI.HiTracking.hiMultiTrackSelector_cfi.hiLooseMTS.clone(
     name = 'hiDetachedLoose',
+    applyAdaptedPVCuts = cms.bool(False)
     ), #end of pset
     RecoHI.HiTracking.hiMultiTrackSelector_cfi.hiTightMTS.clone(
     name = 'hiDetachedTight',
     preFilterName = 'hiDetachedLoose',
+    applyAdaptedPVCuts = cms.bool(False)
     ),
     RecoHI.HiTracking.hiMultiTrackSelector_cfi.hiHighpurityMTS.clone(
     name = 'hiDetachedStep',
     preFilterName = 'hiDetachedTight',
+    applyAdaptedPVCuts = cms.bool(False)
     # min_nhits = 14
     ),
     ) #end of vpset
     ) #end of clone
-
-#check if that is necessary
-hiDetachedSelector.trackSelectors[0].d0_par2=cms.vdouble(99999.0,99999.0)
-hiDetachedSelector.trackSelectors[1].d0_par2=cms.vdouble(99999.0,99999.0)
-hiDetachedSelector.trackSelectors[2].d0_par2=cms.vdouble(99999.0,99999.0)
-
-hiDetachedSelector.trackSelectors[0].dz_par2=cms.vdouble(99999.0,99999.0)
-hiDetachedSelector.trackSelectors[1].dz_par2=cms.vdouble(99999.0,99999.0)
-hiDetachedSelector.trackSelectors[2].dz_par2=cms.vdouble(99999.0,99999.0)
-
-
-
-
-#hidetachedTripletQual.selectedTrackQuals=cms.VInputTag(cms.InputTag("hidetachedTripletStepSelector","hidetachedTripletStepVtx"), 
-#                                                               cms.InputTag("hidetachedTripletStepSelector","hidetachedTripletStepTrk"))
-#hidetachedTripletQual.TrackProducers=cms.VInputTag(cms.InputTag("hidetachedTripletStepTracks"), cms.InputTag("hidetachedTripletStepTracks"))
-
-
-
 
 import RecoTracker.FinalTrackSelectors.trackListMerger_cfi
 hiDetachedQual = RecoTracker.FinalTrackSelectors.trackListMerger_cfi.trackListMerger.clone(
@@ -277,13 +199,11 @@ hiDetachedQual = RecoTracker.FinalTrackSelectors.trackListMerger_cfi.trackListMe
     selectedTrackQuals = cms.VInputTag(cms.InputTag("hiSecondPixelTripletStepSelector","hiDetachedHighPurity")),
     copyExtras = True,
     makeReKeyedSeeds = cms.untracked.bool(False),
-    #writeOnlyTrkQuals = True
     )
 
 
 hiDetachedStep = cms.Sequence(hiDetachedClusters*
                                      hiDetachedSeedLayers*
-                                     #hidetachedTripletStepSeeds*
                                      hiDetachedPixelTracks*
                                      hiDetachedSeeds*
                                      hiDetachedTrackCandidates*
